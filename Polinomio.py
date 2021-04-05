643f033937574c9a2655bb9e0f739eab33a03aec
David Martínez Jiménez <49657007+dmj619@users.noreply.github.com>
2021-04-05 12:58:44 +0200

    updated codesmells

--- Polinomio.py
+++ Polinomio.py
@@ -3,27 +3,27 @@ class Polinomio(list):
     def __add__(self, other):
         '''Overloaded Method Sum. Return the sum of itself and other polynomial'''
         if len(self) > len(other):
-            polinomioSolucion = self[:]
-            for x in range(len(other)): polinomioSolucion[x] += other[x]
+            PolinomioSolucion = self[:]
+            for x in range(len(other)): PolinomioSolucion[x] += other[x]
         else:
-            polinomioSolucion = other[:]
-            for x in range(len(self)): polinomioSolucion[x] += self[x]
-        return polinomioSolucion
+            PolinomioSolucion = other[:]
+            for x in range(len(self)): PolinomioSolucion[x] += self[x]
+        return PolinomioSolucion
 
     def __sub__(self, other):
         '''Overloaded Method Rest. Return the sum of itself and other polynomial'''
         if len(self) > len(other):
-            polinomioSolucion = self[:]
-            for x in range(len(other)): polinomioSolucion[x] += other[x]
+            PolinomioSolucion = self[:]
+            for x in range(len(other)): PolinomioSolucion[x] += other[x]
         else:
-            polinomioSolucion = other[:]
-            for x in range(len(self)): polinomioSolucion[x] += self[x]
-        return polinomioSolucion
+            PolinomioSolucion = other[:]
+            for x in range(len(self)): PolinomioSolucion[x] += self[x]
+        return PolinomioSolucion
 
     def __mul__(self, other):
         ''''Overloaded Method Product. Return the product of itself and other polynomial'''
-        polinomioSolucion = [0]*(len(self)+len(other)-1)
+        PolinomioSolucion = [0]*(len(self)+len(other)-1)
         for pos1, val1 in enumerate(self):
             for pos2, val2 in enumerate(other):
-                polinomioSolucion[pos1+pos2] += val1*val2
-        return polinomioSolucion
+                PolinomioSolucion[pos1+pos2] += val1*val2
+        return PolinomioSolucion

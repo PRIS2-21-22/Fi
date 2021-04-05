cf811d5721231fbd346bae191996db24f71f9fcf
David Martínez Jiménez <49657007+dmj619@users.noreply.github.com>
2021-04-05 13:00:23 +0200

    Add files via upload

--- Polinomio.py
+++ Polinomio.py
@@ -3,27 +3,27 @@ class Polinomio(list):
     def __add__(self, other):
         '''Overloaded Method Sum. Return the sum of itself and other polynomial'''
         if len(self) > len(other):
-            PolinomioSolucion = self[:]
-            for x in range(len(other)): PolinomioSolucion[x] += other[x]
+            solucion = self[:]
+            for x in range(len(other)): solucion[x] += other[x]
         else:
-            PolinomioSolucion = other[:]
-            for x in range(len(self)): PolinomioSolucion[x] += self[x]
-        return PolinomioSolucion
+            solucion = other[:]
+            for x in range(len(self)): solucion[x] += self[x]
+        return solucion
 
     def __sub__(self, other):
         '''Overloaded Method Rest. Return the sum of itself and other polynomial'''
         if len(self) > len(other):
-            PolinomioSolucion = self[:]
-            for x in range(len(other)): PolinomioSolucion[x] += other[x]
+            solucion = self[:]
+            for x in range(len(other)): solucion[x] += other[x]
         else:
-            PolinomioSolucion = other[:]
-            for x in range(len(self)): PolinomioSolucion[x] += self[x]
-        return PolinomioSolucion
+            solucion = other[:]
+            for x in range(len(self)): solucion[x] += self[x]
+        return solucion
 
     def __mul__(self, other):
         ''''Overloaded Method Product. Return the product of itself and other polynomial'''
-        PolinomioSolucion = [0]*(len(self)+len(other)-1)
+        solucion = [0]*(len(self)+len(other)-1)
         for pos1, val1 in enumerate(self):
             for pos2, val2 in enumerate(other):
-                PolinomioSolucion[pos1+pos2] += val1*val2
-        return PolinomioSolucion
+                solucion[pos1+pos2] += val1*val2
+        return solucion
